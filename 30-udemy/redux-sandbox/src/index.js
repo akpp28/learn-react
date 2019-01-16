4aa34ada8a573b5b6fa22df51ef977463ef0c7c6
Akrapovich <akkrapovich@gmail.com>
2019-01-16 22:09:14 +0200

    udemy: redux: bindActionCreators

--- 30-udemy/redux-sandbox/src/index.js
+++ 30-udemy/redux-sandbox/src/index.js
@@ -1,32 +1,36 @@
-import {createStore} from 'redux';
+import {bindActionCreators, createStore} from 'redux';
+import reducer from "./reducer";
+import * as actions from "./actions";
 
-function reducer(state = 0, action) {
-    switch (action.type) {
-        case 'INC':
-            return state + 1;
-        case 'DEC':
-            return state - 1;
-        case 'RND':
-            return state + action.payload;
-        default:
-            return 0;
-    }
-}
 
-const store = createStore(reducer);
-const inc = () => ({type: 'INC'});
-const dec = () => ({type: 'DEC'});
-const rnd = (payload) => ({type: 'RND', payload});
+export const store = createStore(reducer);
+const {dispatch} = store;
+/*
+const bindActionCreators = (creator, dispatch) => (...args) => dispatch(creator(...args));
 
-document.getElementById('inc').addEventListener('click', ()=>{
-    store.dispatch(inc());
-});
-document.getElementById('dec').addEventListener('click', ()=>{
-    store.dispatch(dec());
-});
-document.getElementById('rnd').addEventListener('click', ()=>{
-    const payload = Math.floor(Math.random()*10);
-    store.dispatch(rnd(payload));
+const incDispatch = () => bindActionCreators(inc, dispatch);
+const decDispatch = () => bindActionCreators(dec, dispatch);
+const rndDispatch = () => bindActionCreators(rnd, dispatch)
+*/
+
+/*
+const {incDispatch, decDispatch, rndDispatch} = bindActionCreators({
+    incDispatch: inc,
+    decDispatch: dec,
+    rndDispatch: rnd,
+}, dispatch)
+*/
+
+const {inc, dec, rnd} = bindActionCreators(actions, dispatch);
+
+document
+    .getElementById('inc')
+    .addEventListener('click', inc);
+
+document.getElementById('dec').addEventListener('click', dec);
+document.getElementById('rnd').addEventListener('click', () => {
+    const payload = Math.floor(Math.random() * 10);
+    rnd(payload);
 });
 
 const update = () => {
