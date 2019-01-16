fef9b5abecc6c17fa54324e6d244f89a25b3c331
Akrapovich <akkrapovich@gmail.com>
2019-01-16 12:45:45 +0200

    udemy: redux action creator

--- 30-udemy/redux-sandbox/src/index.js
+++ 30-udemy/redux-sandbox/src/index.js
@@ -6,18 +6,27 @@ function reducer(state = 0, action) {
             return state + 1;
         case 'DEC':
             return state - 1;
+        case 'RND':
+            return state + action.payload;
         default:
             return 0;
     }
 }
 
 const store = createStore(reducer);
+const inc = () => ({type: 'INC'});
+const dec = () => ({type: 'DEC'});
+const rnd = (payload) => ({type: 'RND', payload});
 
 document.getElementById('inc').addEventListener('click', ()=>{
-    store.dispatch({type: 'INC'});
+    store.dispatch(inc());
 });
 document.getElementById('dec').addEventListener('click', ()=>{
-    store.dispatch({type: 'DEC'});
+    store.dispatch(dec());
+});
+document.getElementById('rnd').addEventListener('click', ()=>{
+    const payload = Math.floor(Math.random()*10);
+    store.dispatch(rnd(payload));
 });
 
 const update = () => {
